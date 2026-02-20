188261bedb59ded188f0a0f80ced485a565e5fc7
rajeshmalli0802 <rajeshmalli0802@gmail.com>
2026-02-20 21:17:40 +0530

    Create to_print_cube.cpp

---
+++ to_print_cube.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int cube(int a){
+    return a*a*a;
+}
+int main(){
+    int num;
+    cout<<"Enter Number : ";
+    cin>>num;
+    int result=cube(num);
+    cout<<"Cube of "<<num<<" is "<<result;
+    return 0;
+}
