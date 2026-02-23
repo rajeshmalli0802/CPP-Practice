a192c90e56c92a4cfafc640543dc37628d2431e6
Rajesh Malli <rajeshmalli0802@gmail.com>
2026-02-23 14:03:34 +0530

    Create class_methods_outside_ the_class.cpp

---
+++ class_methods_outside_ the_class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class calculator
+{
+    public:
+    int add(int a, int b);
+};
+
+int calculator::add(int a, int b){
+    return a+b;
+}
+
+
+int main()
+{
+    calculator calc;
+    cout<<" addition: "<<calc.add(15,5) <<endl;
+    return 0;
+}
